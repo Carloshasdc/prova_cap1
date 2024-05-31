f561cbdd70ab0b16d1e9c0ce64090112dab473da
Carlos Assuncao <carloshsdc33@gmail.com>
2024-05-31 09:09:36 -0300

    prova cap1

---
+++ prova cap1/prova_capy1.py
@@ -0,0 +1,4 @@
+base = float(input("Digite a base do retângulo: "))
+altura = float(input("Digite a altura do retângulo: "))
+area_do_retangulo = base*altura
+print(f"A area do retângulo é {area_do_retangulo:.2f}")
